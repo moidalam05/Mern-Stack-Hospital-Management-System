d4e958b9990ee48751fce748322dec4507306344
moidalam05 <moidalam005@hotmail.com>
2024-03-22 08:18:40 +0530

    Add message routes and controller

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
+import messageRoutes from './message.routes.js';
 const router = Router();
+
+router.use('/message', messageRoutes);
 
 export default router;
