776cb1f9d0d03d90f2b666d3cba5feb890f2a98a
moidalam05 <moidalam005@hotmail.com>
2024-03-23 22:36:28 +0530

    Delete unnecessary files and code

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import messageRoutes from './message.routes.js';
-import authRoutes from './auth.routes.js';
-const router = Router();
-
-router.use('/message', messageRoutes);
-router.use('/auth',authRoutes)
-
-export default router;
--- backend/src/routes/message.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendMessage } from '../controllers/message.controllers.js';
-
-const router = Router();
-
-router.post('/', sendMessage);
-
-export default router;
