772f6fde97ea46609b37366a1039e115f3db081e
moidalam05 <moidalam005@hotmail.com>
2024-03-24 16:29:32 +0530

    Add authentication middlewares and protect admin registration route

---
+++ backend/src/middlewares/authentication.js
@@ -0,0 +1,39 @@
+import asyncHandler from './asyncHandler.js';
+import CustomError from './customError.js';
+import jwt from 'jsonwebtoken';
+import config from '../config/index.js';
+import User from '../models/userSchema.js';
+
+export const isAdminLoggedIn = asyncHandler(async (req, res, next) => {
+	const token = req.cookies.adminToken;
+	if (!token) {
+		throw new CustomError('Admin Not authorized to access this route', 401);
+	}
+
+	const decoded = jwt.verify(token, config.JWT_SECRET_KEY);
+	req.user = await User.findById(decoded.id);
+	if (!req.user.role === 'Admin') {
+		throw new CustomError(
+			`${req.user.role} not authorized for this resource!`,
+			403
+		);
+	}
+	next();
+});
+
+export const isPatientLoggedIn = asyncHandler(async (req, res, next) => {
+	const token = req.cookies.patientToken;
+	if (!token) {
+		throw new CustomError('Patient Not authorized to access this route', 401);
+	}
+
+	const decoded = jwt.verify(token, config.JWT_SECRET_KEY);
+	req.user = await User.findById(decoded.id);
+	if (!req.user.role === 'Patient') {
+		throw new CustomError(
+			`${req.user.role} not authorized for this resource!`,
+			403
+		);
+	}
+	next();
+});
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
-import { register, login, logout,adminRegister } from '../controllers/auth.controller.js';
+import {
+	register,
+	login,
+	logout,
+	adminRegister,
+} from '../controllers/auth.controller.js';
+import {
+	isAdminLoggedIn,
+	isPatientLoggedIn,
+} from '../middlewares/authentication.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/admin/register', adminRegister);
+router.post('/admin/register',isAdminLoggedIn, adminRegister);
 router.get('/logout', logout);
 
 export default router;
