ee91676c28dcbfd8bb2d39cb598570776c1b90a9
moidalam05 <moidalam005@hotmail.com>
2024-03-24 16:50:58 +0530

    Fix token case sensitivity issue in authentication middleware and jwtToken utility

--- backend/src/middlewares/authentication.js
+++ backend/src/middlewares/authentication.js
@@ -5,14 +5,15 @@ import config from '../config/index.js';
 import User from '../models/userSchema.js';
 
 export const isAdminLoggedIn = asyncHandler(async (req, res, next) => {
-	const token = req.cookies.adminToken;
+	const token = req.cookies.AdminToken;
+	console.log(token);
 	if (!token) {
 		throw new CustomError('Admin Not authorized to access this route', 401);
 	}
 
 	const decoded = jwt.verify(token, config.JWT_SECRET_KEY);
 	req.user = await User.findById(decoded.id);
-	if (!req.user.role === 'Admin') {
+	if (req.user.role !== 'Admin') {
 		throw new CustomError(
 			`${req.user.role} not authorized for this resource!`,
 			403
@@ -22,7 +23,7 @@ export const isAdminLoggedIn = asyncHandler(async (req, res, next) => {
 });
 
 export const isPatientLoggedIn = asyncHandler(async (req, res, next) => {
-	const token = req.cookies.patientToken;
+	const token = req.cookies.PatientToken;
 	if (!token) {
 		throw new CustomError('Patient Not authorized to access this route', 401);
 	}
--- backend/src/utils/jwtToken.js
+++ backend/src/utils/jwtToken.js
@@ -2,7 +2,7 @@ import config from '../config/index.js';
 
 export const generateToken = (user, message, statusCode, res) => {
 	const token = user.generateJwtToken();
-	const cookieName = user.role === 'Admin' ? 'adminToken' : 'PatientToken';
+	const cookieName = user.role === 'Admin' ? 'AdminToken' : 'PatientToken';
 	res
 		.status(statusCode)
 		.cookie(cookieName, token, {
@@ -10,7 +10,6 @@ export const generateToken = (user, message, statusCode, res) => {
 				Date.now() + config.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
 			),
 			httpOnly: true,
-			secure: true,
 		})
 		.json({
 			success: true,
